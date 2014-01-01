187660c5aeaf6952ce01ff02f21a2f268501c4c2
Yeuk Hon Wong <yeukhon@acm.org>
2014-01-01 20:13:22 +0000

    Initialized basic plugin repo structure.

---
+++ setup.py
@@ -0,0 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup
+
+install_requires = []
+
+setup(name="minion-wpscan-plugin",
+      version="0.1",
+      description="wpscan plugin for Minion",
+      url="https://github.com/yeukhon/minion-wpscan-plugin/",
+      author="Yeuk Hon Wong",
+      author_email="yeukhon@acm.org",
+      packages=['minion', 'minion.plugins', 'minion.plugins.wpscan'],
+      namespace_packages=['minion', 'minion.plugins'],
+      include_package_data=True,
+      install_requires=install_requires)
